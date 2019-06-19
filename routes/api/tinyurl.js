ed84efe675ccbd3e1b9e5780a80c39f445002daf
Ronson Wuniardy <rwuniard@hotmail.com>
2019-06-18 23:47:59 -0400

    Added reverseTinyURL

--- routes/api/tinyurl.js
+++ routes/api/tinyurl.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const map = new String('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 
 //Set REST API
-router.get('/', (req, res) => {
+router.get('/getTinyUrl', (req, res) => {
     var url = idToShortURL(12345);
 
     console.log(url);
     res.json(url);
+
+})
+
+router.get('/getId', (req, res) => {
+    var id = reverseTinyURL('dnh');
+
+    console.log(id);
+    res.json(id);
 
 })
 
@@ -19,8 +27,35 @@ function idToShortURL(id) {
         id = id / 62;
     }
     console.log(tinyUrl);
-    return tinyUrl.split('').reverse().join();
+    return tinyUrl.split('').reverse().join('');
+}
 
+function reverseTinyURL(tinyUrl) {
+    id = 0;
+    for (i=0; i< tinyUrl.length; i++) {
+        if ('a'.charCodeAt(0) <= tinyUrl.charCodeAt(i) && tinyUrl.charCodeAt(i) <= 'z'.charCodeAt(0)) {
+            id = (id * 62) + tinyUrl.charCodeAt(i) - 'a'.charCodeAt(0);
+        }
+        if ('A'.charCodeAt(0) <= tinyUrl.charCodeAt(i) && tinyUrl.charCodeAt(i) <= 'Z'.charCodeAt(0)) {
+            id = (id *62) + tinyUrl.charCodeAt(i) - 'Z'.charCodeAt(0);
+        }
+        if ('0'.charCodeAt(0) <= tinyUrl.charCodeAt(i) && tinyUrl.charCodeAt(i) <= '9'.charCodeAt(0)) {
+            id = (id *62) + tinyUrl.charCodeAt(i) - '0'.charCodeAt(0);
+        }
+
+    }
+    return id;
+    // int id = 0;
+    // for (int i = 0; i < tinyURL.length(); i++) {
+
+    //     if ('a' <= tinyURL.charAt(i) && tinyURL.charAt(i) <= 'z')
+    //         id = (id * 62) + tinyURL.charAt(i) - 'a';
+    //     if ('A' <= tinyURL.charAt(i) && tinyURL.charAt(i) <= 'Z')
+    //         id = (id * 62) + tinyURL.charAt(i) - 'Z';
+    //     if ('0' <= tinyURL.charAt(i) && tinyURL.charAt(i) <= '9')
+    //         id = (id * 62) + tinyURL.charAt(i) - '0';
+    // }
+    // return id;
 }
 
 
