d575bc20fcc7975f7f5869ed09ec57a5ac93b490
Ronson Wuniardy <rwuniard@hotmail.com>
2019-06-18 23:22:10 -0400

    adding idToShortURL

--- routes/api/tinyurl.js
+++ routes/api/tinyurl.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const map = new String('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 
 //Set REST API
 router.get('/', (req, res) => {
-    console.log("get rest api");
-    res.json('{msg: "true"}');
+    var url = idToShortURL(12345);
+
+    console.log(url);
+    res.json(url);
 
 })
+
+function idToShortURL(id) {
+    var tinyUrl = "";
+    while (id > 1) {
+        n = id%62;
+        tinyUrl+=map.charAt(n);
+        id = id / 62;
+    }
+    console.log(tinyUrl);
+    return tinyUrl.split('').reverse().join();
+
+}
+
 
 module.exports = router;
